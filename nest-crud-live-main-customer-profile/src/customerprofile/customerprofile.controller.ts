d96f51abfda66707d7624d845768ae54ef986790
sanketcalfus <sanket.kawale@calfus.com>
2024-02-11 15:25:49 +0530

    TF2-77: swagger changes and code optimization

--- nest-crud-live-main-customer-profile/src/customerprofile/customerprofile.controller.ts
+++ nest-crud-live-main-customer-profile/src/customerprofile/customerprofile.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { CustomerprofileService } from './customerprofile.service';
 import { CustomerProfile } from './customerprofile.entity';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('customerprofile')
 export class CustomerprofileController {
@@ -18,18 +19,43 @@ export class CustomerprofileController {
 
   // Get all customer profiles
   @Get()
+  @ApiOperation({ summary: 'Get all customer profiles' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of customer profiles',
+    type: [CustomerProfile],
+  })
   async getAllCustomerProfile(): Promise<CustomerProfile[]> {
     return await this.customerProfileService.getAllCustomerProfile();
   }
 
-  // Get one customer profile
-  @Get(':id')
+  // Get one customer profile using profile_id or customer_id
+  @Get('/:type/:id')
+  @ApiOperation({ summary: 'Get a customer profile by type and ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The customer profile',
+    type: CustomerProfile,
+  })
+  @ApiParam({
+    name: 'type',
+    enum: ['profile_id', 'customer_id'],
+    type: 'string',
+  })
+  @ApiParam({ name: 'id', type: 'string' })
   async getCustomerProfile(
-    @Param('id') profile_id: string,
+    @Param('type') type: 'profile_id' | 'customer_id',
+    @Param('id') id: string,
   ): Promise<CustomerProfile> {
     try {
-      const profile =
-        this.customerProfileService.getCustomerProfile(profile_id);
+      let profile: CustomerProfile;
+      if (type === 'profile_id') {
+        profile =
+          await this.customerProfileService.getCustomerProfileByProfileId(id);
+      } else {
+        profile =
+          await this.customerProfileService.getCustomerProfileByCustomerId(id);
+      }
       if (!profile) {
         throw new Error('User Profile Not found!');
       } else {
@@ -42,6 +68,13 @@ export class CustomerprofileController {
 
   // Create customer profile
   @Post()
+  @ApiOperation({ summary: 'Create a new customer profile' })
+  @ApiResponse({
+    status: 201,
+    description: 'The newly created customer profile',
+    type: CustomerProfile,
+  })
+  @ApiBody({ type: CustomerProfile })
   async createCustomerProfile(
     @Body() profile: CustomerProfile,
   ): Promise<CustomerProfile> {
@@ -54,6 +87,17 @@ export class CustomerprofileController {
 
   // Update customer profile
   @Put(':id')
+  @ApiOperation({ summary: 'Update a customer profile by profile_id' })
+  @ApiParam({ name: 'id', description: 'Customer Profile ID', type: 'string' })
+  @ApiBody({
+    type: CustomerProfile,
+    description: 'Updated customer profile data',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Customer profile updated successfully',
+    type: CustomerProfile,
+  })
   async updateCustomerProfile(
     @Param('id') profile_id: string,
     @Body() profile: CustomerProfile,
@@ -70,13 +114,17 @@ export class CustomerprofileController {
 
   // Delete customer profile
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a customer profile by ID' })
+  @ApiParam({ name: 'id', description: 'Profile ID to delete', type: 'string' })
+  @ApiResponse({
+    status: 200,
+    description: 'Customer profile deleted successfully',
+  })
   async deleteCustomerProfile(@Param('id') profile_id: string): Promise<void> {
     try {
-      const profile =
-        this.customerProfileService.getCustomerProfile(profile_id);
-      if (!profile) {
-        throw new Error('Customer profile not found');
-      }
+      await this.customerProfileService.getCustomerProfileByProfileId(
+        profile_id,
+      );
       this.customerProfileService.deleteCustomerProfile(profile_id);
     } catch (error) {
       return error;
