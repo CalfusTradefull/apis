1e48af737f2f88214c17e2442e2d91614c9f3bfc
hbbhadraa <158356407+hbbhadraa@users.noreply.github.com>
2024-02-12 15:44:02 -0800

    Delete src directory

--- src/config/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
-export const AppConfig = {
-    DAL_URL: "http://localhost:3000/",
-    APIVERSION: "v1.0.0",
-    DESCRIPTION: "TF Head Less APIs",
-  };
